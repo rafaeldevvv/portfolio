c504a41e344d96e8d8bd88fab568c2c663589ec8
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2023-07-18 22:05:26 -0300

    Commit src files

---
+++ src/projects.js
@@ -0,0 +1,34 @@
+export default [
+   {
+      heading: "Snake Game",
+      repoUrl: '',
+      liveSiteUrl: '',
+      description: '',
+      image: '',
+      favorite: true,
+   },
+   {
+      heading: "URL Shortener",
+      repoUrl: '',
+      liveSiteUrl: '',
+      description: '',
+      image: '',
+      favorite: false,
+   },
+   {
+      heading: "Tic Tac Toe",
+      repoUrl: '',
+      liveSiteUrl: '',
+      description: '',
+      image: '',
+      favorite: true,
+   },
+   {
+      heading: "Countries RESTful API",
+      repoUrl: '',
+      liveSiteUrl: '',
+      description: '',
+      image: '',
+      favorite: true,
+   },
+]
---
+++ src/skills.js
@@ -0,0 +1,46 @@
+const skills = [
+   {
+      name: "HTML5",
+      initialDate: "10/20/2022",
+      icon: "fa-brands fa-html5",
+      className: "html",
+      knowledgePercentage: 80,
+   },
+   {
+      name: "CSS3",
+      initialDate: "10/20/2022",
+      icon: "fa-brands fa-css3",
+      className: "css",
+      knowledgePercentage: 85,
+   },
+   {
+      name: "JavaScript",
+      initialDate: "12/17/2022",
+      icon: "fa-brands fa-js",
+      className: "javascript",
+      knowledgePercentage: 85,
+   },
+   {
+      name: "React.js",
+      initialDate: "02/01/2023",
+      icon: "fa-brands fa-react",
+      className: "react",
+      knowledgePercentage: 95,
+   },
+   {
+      name: "SASS",
+      initialDate: "10/20/2022",
+      icon: "fa-brands fa-sass",
+      className: "sass",
+      knowledgePercentage: 70,
+   },
+   {
+      name: "Git/GitHub",
+      initialDate: "12/08/2022",
+      icon: "fa-brands fa-github",
+      className: "github",
+      knowledgePercentage: 50,
+   },
+];
+
+export default skills;
