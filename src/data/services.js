abe64e77e1bfadb56403e7b38e512569a637e161
rafaeldevvv <115797599+rafaeldevvv@users.noreply.github.com>
2023-07-30 19:17:04 -0300

    Add font awesome icon to services data

--- src/data/services.js
+++ src/data/services.js
@@ -2,52 +2,63 @@ const services = [
    {
       name: "Landing Pages & Homepages",
       description: "Hey there! Ready to take your brand to new heights? I'm all about crafting captivating design that makes your website shine. Let's create an online presence that leaves a lasting impression and truly engages your audience. Reach out to me, and together, we'll make your vision a reality. Can't wait to bring your website to life!",
-      tags: ["landing page", "homepage"]
+      tags: ["landing page", "homepage"],
+      fontAwesomeIcon: "fa-solid fa-plane-arrival"
    },
    {
       name: "Single Page Applications - Client-side",
       description: "Looking to amp up your web experience? My Single Page Application (SPA) service is just what you need. Say goodbye to page reloads and hello to seamless performance. Let's build a dynamic user experience that keeps visitors hooked. Reach out, and we'll create a client-side magic that'll leave your users impressed.",
-      tags: ["spa", "single page application"]
+      tags: ["spa", "single page application"],
+      fontAwesomeIcon: "fa-solid fa-computer"
    },
    {
       name: "Static Web Apps",
       description: "Simple and stunning web apps with client-side magic. Using HTML, CSS, and JavaScript, I'll craft a seamless user experience. No server-side complexities, just elegant content presentation. Let's bring your vision to life. Reach out to get started!",
-      tags: ["static web app"]
+      tags: ["static web app"],
+      fontAwesomeIcon: "fa-solid fa-newspaper"
    },
    {
       name: "Responsive Design",
       description: "Your website should look great on ALL devices, and I'm here to make it happen. With responsive design, your content will adapt flawlessly to any screen size. Whether it's a desktop, tablet, or smartphone, your users will have an optimal experience. Reach out, and let's create a responsive masterpiece together!",
-      tags: ["responsive"]
+      tags: ["responsive"],
+      fontAwesomeIcon: "fa-solid fa-tablet-screen-button"
    },
    {
       name: "Frontend Performance Optimization",
       description: "Need a blazing-fast website? My Frontend Performance Optimization service is just what you're looking for. I'll fine-tune every aspect of your front-end code to ensure lightning-fast loading times and seamless user interactions. Say hello to a high-performance web presence that leaves your competitors in the dust!",
-      tags: ['performance']
+      tags: ['performance'],
+      fontAwesomeIcon: "fa-solid fa-laptop-code"
    },
    {
       name: "Cross-browser Compatibility",
       description: "Ever seen a website work perfectly in one browser but fail in others? Not anymore! With my Cross-Browser Compatibility service, I'll fix those issues and ensure your website runs smoothly across all browsers.",
-      tags: ["cross-browser"]
+      tags: ["cross-browser"],
+      fontAwesomeIcon: "fa-brands fa-firefox"
    },
    {
       name: "Web Accessibility",
       description: "The web should be accessible to everyone, and I'm here to make it happen. With my Web Accessibility service, I'll ensure your website is inclusive and user-friendly for everyone, including individuals with disabilities. Let's create an online space that caters to all users, regardless of their abilities.",
-      tags: ["accessibility"]
+      tags: ["accessibility"],
+      fontAwesomeIcon: "fa-brands fa-accessible-icon"
+      /* try "fa-solid fa-universal-access" otherwise */
    },
    {
       name: "Integration with APIs",
       description: "Already have an API on the back-end or a team working on it? I've got your front-end covered! With my Integration with APIs service, I'll seamlessly connect your front-end code to the existing API. Whether it's fetching data, handling user interactions, or displaying dynamic content, I'll ensure a smooth connection between front-end and back-end.",
-      tags: ["api"]
+      tags: ["api"],
+      fontAwesomeIcon: "fa-solid fa-microchip"
    },
    {
       name: "Portfolio",
       description: "Ready to showcase your skills and impress clients and employers? I'm here to make it happen! With my Portfolio service, we'll create a personalized showcase of your projects and accomplishments. Let's highlight your expertise, experience, and creativity in one stunning portfolio. Whether you're a developer, designer, or multi-talented professional, I'll craft a portfolio that sets you apart!",
-      tags: ["portfolio"]
+      tags: ["portfolio"],
+      fontAwesomeIcon: "fa-solid fa-briefcase"
    },
    {
       name: "Personal Webpage",
       description: "Ever wanted a website that's uniquely you? Look no further! With my Personal Webpage service, I'll build a custom website that reflects your individuality and showcases your talents. Let's create a digital space that truly represents who you are and what you stand for. Whether it's a personal blog, an online resume, or a creative showcase, I'll tailor it to your preferences and vision.",
-      tags: ['personal page', "portfolio"]
+      tags: ['personal page', "portfolio"],
+      fontAwesomeIcon: "fa-solid fa-blog"
    }
 ];
 
